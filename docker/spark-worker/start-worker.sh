35d7f0c7f73a88e1e0f3112ed1962d41ed15aaf8
daiquan <qdai-korewireless>
2019-07-18 00:40:57 -0500

    split docker-compose file to smaller ones increase core and memory for workers

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -4,4 +4,4 @@
 mkdir -p $SPARK_WORKER_LOG
 export SPARK_HOME=/spark
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
-/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER --cores 4 --memory 2g >> $SPARK_WORKER_LOG/spark-worker.out
--- submit.sh
+++ submit.sh
@@ -1,4 +1,7 @@
  #!/bin/bash
  docker run --network docker-spark_spark-network -v d:/repository/com/quan/sparkApp/1.0-SNAPSHOT:/opt/spark-apps spark-submit:2.4.3
  
- docker run --network docker-spark_spark-network -v d:/repository/com/kore/bling/engine/services/services-worker/1.0-SNAPSHOT:/opt/spark-apps spark-submit
+ docker run --network docker-spark_spark-network -v d:/repository/com/kore/bling/engine/services/services-worker/1.0-SNAPSHOT:/opt/spark-apps spark-submit:2.4.3
+ 
+ 
+ docker network create docker-spark_spark-network --driver=bridge --subnet=10.5.0.0/16
