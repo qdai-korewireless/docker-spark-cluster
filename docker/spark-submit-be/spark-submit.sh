2687883135000b72d22676ba72a1a02be10e6f24
daiquan <qdai-korewireless>
2019-07-20 01:22:23 -0500

    allow submitting with config encoded with base64

--- docker/spark-submit-be/spark-submit.sh
+++ docker/spark-submit-be/spark-submit.sh
@@ -1,5 +1,12 @@
  #!/bin/bash
  
+ #encode overiding config to base64 and pass to application argument -config
+ SPARK_APPLICATION_ARGS=$(/bin/base64 /config.txt)
+ 
+ #replace argument space with empty string, otherwise the spark submitting will fail
+ SPARK_APPLICATION_ARGS=$(echo $SPARK_APPLICATION_ARGS | sed -e "s/ //g")
+ echo ${SPARK_APPLICATION_ARGS}
+
 /spark/bin/spark-submit \
 --class ${SPARK_APPLICATION_MAIN_CLASS} \
 --master ${SPARK_MASTER_URL} \
@@ -8,4 +15,4 @@
 --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3 \
  ${SPARK_SUBMIT_ARGS} \
  ${SPARK_APPLICATION_JAR_LOCATION} \
- ${SPARK_APPLICATION_ARGS} \
+ -config ${SPARK_APPLICATION_ARGS} \
