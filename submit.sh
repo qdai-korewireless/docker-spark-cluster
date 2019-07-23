d7baee681463095f4b52006af119c3b0dae8ddeb
daiquan <qdai-korewireless>
2019-07-23 14:29:58 -0500

    allow pass jar version

--- submit.sh
+++ submit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+v=$1
+
 docker build -t spark-submit ./docker/spark-submit
 
-docker run --network docker-spark_spark-network -v d:/dev/Local-Dockers/docker-spark/env/bling_eng_jar:/tmp/spark-apps spark-submit
+docker run --network docker-spark_spark-network -v d:/dev/Local-Dockers/docker-spark/env/bling_eng_jar:/tmp/spark-apps -e BLING_ENGINE_VERSION=$v spark-submit 
