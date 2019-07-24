57769feabc10b044f88194a8520ed76637e72dd7
daiquan <qdai-korewireless>
2019-07-23 23:07:06 -0500

    import .env to sh file

--- startup.sh
+++ startup.sh
@@ -1,9 +1,16 @@
 
-#build images
+#build spark images
 ./env/build-images.sh
 
+
+#import all env variables
+for var in `cat .env`
+do
+   eval $var
+done
+
 #create a network if not exist
-docker network ls|grep bling-network > /dev/null || docker network create bling-network --driver=bridge --subnet=10.5.0.0/16
+docker network ls|grep ${NETWORK_NAME} > /dev/null || docker network create ${NETWORK_NAME} --driver=bridge --subnet=10.5.0.0/16
 
 #launch everything
 docker-compose -f ./env/backend.yml up -d 
--- submit.sh
+++ submit.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 v=$1
-BLING_ENG_LOCAL_JAR="c:/bling_eng_jar"
+if [ -z "$v" ];
+then
+  echo "version number is required (e.g. 1.1.111)"
+  sleep 5
+  exit
+fi
+
+#import all env variables
+for var in `cat .env`
+do
+   eval $var
+done
 
 #build image
 docker build -t spark-submit ./docker/spark-submit
 
-#create directory for bling engine jar
-mkdir -p ${BLING_ENG_LOCAL_JAR}
-
 #run spark submit container
-docker run --network bling-network -v ${BLING_ENG_LOCAL_JAR}:/tmp/spark-apps -e JAR_VERSION=$v spark-submit 
+docker run --network ${NETWORK_NAME} -v ${BLING_ENG_LOCAL_JAR}:/tmp/spark-apps -e JAR_VERSION=$v spark-submit 
 
