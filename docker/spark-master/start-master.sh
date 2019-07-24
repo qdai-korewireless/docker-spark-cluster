b5507d515c56493297b85b991f74b9a6a59c8840
daiquan <qdai-korewireless>
2019-07-23 23:45:09 -0500

    clean up unused files

--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -4,6 +4,5 @@ export SPARK_MASTER_HOST=`hostname`
 . "/spark/bin/load-spark-env.sh"
 mkdir -p $SPARK_MASTER_LOG
 export SPARK_HOME=/spark
-export SPARK_WORKER_CORES=4
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
